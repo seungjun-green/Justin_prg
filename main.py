bbf61143bedbadbd2decc19ee8e95268395db6ec
SeungJunLee-Apple <lsj3285007@gmail.com>
2022-03-15 01:28:54 +0900

    feature improvements

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ def get_type():
     else:
         return None
 
-
 def send_tweet(order, a,b,c,d,e):
     result = ""
     count = 0
@@ -45,6 +44,7 @@ def send_tweet(order, a,b,c,d,e):
             frequency_penalty=d,
             presence_penalty=e
         )
+
         count += 1
         result += response['choices'][0]['text']
         order += response['choices'][0]['text']
@@ -70,17 +70,58 @@ def send_tweet(order, a,b,c,d,e):
         # if result is shorter than 280 characters
         else:
             api.update_status(result)
-        print('tweeted!')
-    except twitter.errors as e:
-        print(f"Error Happened: {e} \n")
+        print('tweet-tweeted!')
+    except twitter.errors.TweepyException as e:
+        print(f"Tweet - Error Happened: {e} \n")
 
-def tweet(type):
+def send_reply(order,curr_id, user):
+    result = ""
+    count = 0
+    # get the response
+    while True:
+        response = openai.Completion.create(
+            engine="text-davinci-001",
+            prompt=order,
+            temperature=0.5,
+            max_tokens=60,
+            top_p=1,
+            frequency_penalty=0.5,
+            presence_penalty=0,
+            stop=["You:"]
+        )
 
-    orders, settings = helper.create_order(type)
-    a,b,c,d,e = settings
+        count += 1
+        result += response['choices'][0]['text']
+        order += response['choices'][0]['text']
+        if count == 3 or response['choices'][0]['text'] == '':
+            break
 
-    for order in orders:
-        send_tweet(order,a,b,c,d,e)
+    # tweet the result
+    result = f'@{user}' + ' ' + result
+    result = re.sub('\n', '', result)
+    result=process_str(result)
+    print(f"{count}, the reply is {result}")
+    try:
+        if len(result) > 280:
+            split_strings = []
+            for index in range(0, len(result), 270):
+                split_strings.append(result[index: index + 270])
+
+            for i, str in enumerate(split_strings):
+                if i == 0:
+                    api.update_status(status=str, in_reply_to_status_id=curr_id)
+
+                else:
+                    result = api.user_timeline(user_id='justin_prg', count=1)
+                    recent_id = result[0]._json['id']
+                    api.update_status(status=str, in_reply_to_status_id=recent_id)
+
+        # if result is shorter than 280 characters
+        else:
+            api.update_status(status=result, in_reply_to_status_id=curr_id)
+            print("reply-tweeted!")
+    except twitter.errors.TweepyException as e:
+        print(f"reply-Error Happened {e}\n")
 
 
 def creating_contents():
@@ -92,21 +133,13 @@ def creating_contents():
             pass
         time.sleep(60)
 
-def construct_reply(tw_id):
-    order = construct_order(tw_id)
+def tweet(type):
+    orders, settings = helper.create_order(type)
+    a,b,c,d,e = settings
 
-    response = openai.Completion.create(
-        engine="text-davinci-001",
-        prompt=order,
-        temperature=0.5,
-        max_tokens=60,
-        top_p=1,
-        frequency_penalty=0.5,
-        presence_penalty=0,
-        stop=["You:"]
-    )
+    for order in orders:
+        send_tweet(order,a,b,c,d,e)
 
-    return response['choices'][0]['text']
 
 
 def construct_order(tw_id):
@@ -140,65 +173,72 @@ def construct_order(tw_id):
     chats.reverse()
     order = ""
     for chat in chats:
+        chat = re.sub('\n', '', chat)
         chat += '\n'
         order += chat
 
-    order+='\n'
     order+='You:'
+    print("-------start of the order-------")
     print(order)
+    print("-------end of the order-------")
     return order
 
-
-def reply_or_like():
+class Data:
     firstTime = True
     lastReplied_id = 0
 
+def get_replies():
+    replies = []
+    if Data.firstTime:
+        rd = api.mentions_timeline(count=1)
+        print(f"first Time: {len(rd)}")
+        for dot in rd:
+            replies.append((dot._json['id'], dot._json['text'], dot._json['user']['screen_name']))
+        Data.firstTime = False
+    else:
+        rd = api.mentions_timeline(since_id=Data.lastReplied_id)
+        print(f"second Time: {len(rd)}")
+        for dot in rd:
+            replies.append((dot._json['id'], dot._json['text'], dot._json['user']['screen_name']))
+
+    return replies
+
+
+def process_str(str):
+    last = str.find('Friend:')
+    if last==-1:
+        return str
+    else:
+        return str[:last]
+
+def reply():
     while True:
-        replies = []
-        if firstTime:
-            rd = api.mentions_timeline(count=1)
-            print(f"first Time: {len(rd)}")
-            for dot in rd:
-                replies.append((dot._json['id'], dot._json['text'], dot._json['user']['screen_name']))
-            firstTime=False
-        else:
-            rd = api.mentions_timeline(since_id=lastReplied_id)
-            print(f"second Time: {len(rd)}")
-            for dot in rd:
-                replies.append((dot._json['id'], dot._json['text'], dot._json['user']['screen_name']))
+        replies = get_replies()
 
         for i, reply in enumerate(replies):
             curr_id,text, user = reply
-            my_reply = construct_reply(curr_id)
-            my_reply = f'@{user}'+' ' + my_reply
-            print(f"the reply is {my_reply}")
             try:
-                api.update_status(status=my_reply, in_reply_to_status_id=curr_id)
-            except:
-                print("already replied")
+                send_reply(construct_order(curr_id),curr_id, user)
+            except twitter.errors.TweepyException as e:
+                print(f"reply func- Error {e} \n")
 
             if i==0:
-                lastReplied_id = curr_id
+                Data.lastReplied_id = curr_id
 
         time.sleep(15)
 
 
 if __name__ == "__main__":
     processes = []
-    p1 = multiprocessing.Process(target=creating_contents)
-    p1.start()
-    processes.append(p1)
+    # p1 = multiprocessing.Process(target=creating_contents)
+    # p1.start()
+    # processes.append(p1)
 
-    # p2=multiprocessing.Process(target=reply_or_like)
-    # p2.start()
-    # processes.append(p2)
+    p2=multiprocessing.Process(target=reply)
+    p2.start()
+    processes.append(p2)
 
     for process in processes:
         process.join()
-
-
-
-
-
 
 
