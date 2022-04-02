921e32679914df1b04dd74260829ebc903c0c21e
seungjun-green <lsj3285007@gmail.com>
2022-04-02 18:49:49 +0900

    updated some code:)

--- main.py
+++ main.py
@@ -51,28 +51,20 @@ def elon():
         time.sleep(15)
 
 
-# if __name__ == "__main__":
-#     print("Start of the program")
-#     processes = []
-#     # p1 = multiprocessing.Process(target=tweet)
-#     # p1.start()
-#     # processes.append(p1)
-#     #
-#     # p2=multiprocessing.Process(target=reply)
-#     # p2.start()
-#     # processes.append(p2)
-#
-#     p3 = multiprocessing.Process(target=elon)
-#     p3.start()
-#     processes.append(p3)
-#
-#     for process in processes:
-#         process.join()
+if __name__ == "__main__":
+    print("Start of the program")
+    processes = []
+    p1 = multiprocessing.Process(target=tweet)
+    p1.start()
+    processes.append(p1)
 
+    p2=multiprocessing.Process(target=reply)
+    p2.start()
+    processes.append(p2)
 
+    p3 = multiprocessing.Process(target=elon)
+    p3.start()
+    processes.append(p3)
 
-
-
-
-
-print(rd)
+    for process in processes:
+        process.join()
