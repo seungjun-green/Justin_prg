de0c123ba36f2d553028b63771323e348f445cf5
seungjun-green <lsj3285007@gmail.com>
2022-07-20 20:37:48 +0900

    improved some code

--- Twitter.py
+++ Twitter.py
@@ -1,1 +1,60 @@
-import Twitter as tweepy
+import tweepy as twitter
+from Resources import keys
+
+auth = twitter.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+auth.set_access_token(keys.oa_key, keys.oa_secret)
+api = twitter.API(auth)
+
+record = {
+    "reply": {"firstTime": True, "lastReplied_id": 0},
+    "elonmusk": {"firstTime": True, "lastReplied_id": 0},
+    "engineers_feed": {"firstTime": True, "lastReplied_id": 0},
+    "tim_cook": {"firstTime": True, "lastReplied_id": 0},
+    "lexfridman": {"firstTime": True, "lastReplied_id": 0}
+}
+
+class Twitter:
+    def get_replies(self):
+        replies = []
+        if record["reply"]["firstTime"]:
+            # len(rd) should be 1 all the time,no matter wgat, but due to some error of Twitter API which say there was any no recent tweet, added While True block
+            # from the second time it is OK for such error happening. Because at first time it have to update recent id. Which will be used in second time
+            try:
+                rd = api.mentions_timeline(count=1)
+                print(f"reply-first Time: {len(rd)}")
+                for dot in rd:
+                    replies.append((dot._json['id'], dot._json['text'], dot._json['user']['screen_name']))
+                record["reply"]["firstTime"] = False
+            except twitter.errors.TweepyException as e:
+                print(f"[get_replies] (First time) Twitter Error: {e}\n")
+        else:
+            try:
+                rd = api.mentions_timeline(since_id=record["reply"]["lastReplied_id"])
+                if (len(rd) > 0):
+                    print(f"reply-second Time: {len(rd)}")
+                for dot in rd:
+                    replies.append((dot._json['id'], dot._json['text'], dot._json['user']['screen_name']))
+            except twitter.errors.TweepyException as e:
+                print(f"[get_replies] (Second time) Twitter Error: {e}\n")
+        return replies
+
+
+    def tweet(self, result, curr_id):
+        if len(result) > 280:
+            split_strings = []
+            for index in range(0, len(result), 270):
+                split_strings.append(result[index: index + 270])
+
+            for i, sub_str in enumerate(split_strings):
+                if i == 0:
+                    api.update_status(status=sub_str, in_reply_to_status_id=curr_id)
+
+                else:
+                    result = api.user_timeline(user_id='justin_prg', count=1)
+                    recent_id = result[0]._json['id']
+                    api.update_status(status=sub_str, in_reply_to_status_id=recent_id)
+
+        # if result is shorter than 280 characters
+        else:
+            api.update_status(status=result, in_reply_to_status_id=curr_id)
+            print("reply-tweeted! \n")
--- helper.py
+++ helper.py
@@ -2,21 +2,15 @@ import openai
 import tweepy as twitter
 import re
 from Resources import keys
+import Twitter
 
 production = False
 engine = "text-davinci-002"
 
-record = {
-    "reply": {"firstTime": True, "lastReplied_id": 0},
-    "elonmusk": {"firstTime": True, "lastReplied_id": 0},
-    "engineers_feed": {"firstTime": True, "lastReplied_id": 0},
-    "tim_cook": {"firstTime": True, "lastReplied_id": 0},
-    "lexfridman": {"firstTime": True, "lastReplied_id": 0}
-}
-
 auth = twitter.OAuthHandler(keys.consumer_key, keys.consumer_secret)
 auth.set_access_token(keys.oa_key, keys.oa_secret)
 api = twitter.API(auth)
+
 
 def create_stop_seq(user):
     return [f"{user}:", "You:"]
@@ -63,27 +57,9 @@ def send_reply(order,particpants,curr_id, user):
     # tweet the reply
     if production:
         try:
-            if len(result) > 280:
-                split_strings = []
-                for index in range(0, len(result), 270):
-                    split_strings.append(result[index: index + 270])
-
-                for i, sub_str in enumerate(split_strings):
-                    if i == 0:
-                        api.update_status(status=sub_str, in_reply_to_status_id=curr_id)
-
-                    else:
-                        result = api.user_timeline(user_id='justin_prg', count=1)
-                        recent_id = result[0]._json['id']
-                        api.update_status(status=sub_str, in_reply_to_status_id=recent_id)
-
-            # if result is shorter than 280 characters
-            else:
-                api.update_status(status=result, in_reply_to_status_id=curr_id)
-                print("reply-tweeted! \n")
+            Twitter.Twitter().tweet(result, curr_id)
         except twitter.errors.TweepyException as e:
             print(f"[send_reply] Twitter Error: {e}\n")
-
     else:
         print("reply tweeted - Development mode\n")
 
@@ -125,30 +101,6 @@ def construct_conv_order(tw_id):
     print(order)
     print("-------end of the order-------\n")
     return order, particpants
-
-def get_replies():
-    replies = []
-    if record["reply"]["firstTime"]:
-        # len(rd) should be 1 all the time,no matter wgat, but due to some error of Twitter API which say there was any no recent tweet, added While True block
-        # from the second time it is OK for such error happening. Because at first time it have to update recent id. Which will be used in second time
-        try:
-            rd = api.mentions_timeline(count=1)
-            print(f"reply-first Time: {len(rd)}")
-            for dot in rd:
-                replies.append((dot._json['id'], dot._json['text'], dot._json['user']['screen_name']))
-            record["reply"]["firstTime"] = False
-        except twitter.errors.TweepyException as e:
-            print(f"[get_replies] (First time) Twitter Error: {e}\n")
-    else:
-        try:
-            rd = api.mentions_timeline(since_id=record["reply"]["lastReplied_id"])
-            if (len(rd) > 0):
-                print(f"reply-second Time: {len(rd)}")
-            for dot in rd:
-                replies.append((dot._json['id'], dot._json['text'], dot._json['user']['screen_name']))
-        except twitter.errors.TweepyException as e:
-            print(f"[get_replies] (Second time) Twitter Error: {e}\n")
-    return replies
 
 def process_str(str):
     last = str.find('Friend:')
