ce830416afccf3daf398156855f151652638fca1
seungjun-green <lsj3285007@gmail.com>
2022-07-31 13:27:32 +0900

    suspended content creating

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ def reply():
 if __name__ == "__main__":
     print("Start of the program\n\n")
     processes = []
-    p1 = multiprocessing.Process(target=tweet)
-    p1.start()
-    processes.append(p1)
+    # p1 = multiprocessing.Process(target=tweet)
+    # p1.start()
+    # processes.append(p1)
 
     p2=multiprocessing.Process(target=reply)
     p2.start()
